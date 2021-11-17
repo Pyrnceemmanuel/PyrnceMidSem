1aaf26108880fd28d48782092f742297cc1bdb1e
Pyrnceemmanuel <91204126+Pyrnceemmanuel@users.noreply.github.com>
2021-11-17 22:59:17 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator}from "@react-navigation/stack";
+import Login from "./Screens/Login";
+import SignUp from "./Screens/SignUp";
+import Home from "./Screens/Home";
+import  ProductDetails  from './Screens/ProductDetails';
+import  Cart  from './Screens/Cart';
+import  CartCount  from './Constants/CartCount';
+import  CartContext  from './Constants/CartContext'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  const MainNavigator = createStackNavigator ();    
+
+return(     
+  <View style={{ flex: 1}} >       
+    <NavigationContainer>         
+      <MainNavigator.Navigator initailRouteName="Login">           
+      <MainNavigator.Screen name="Login" component={Login}/>           
+      <MainNavigator.Screen name="Home" component={Home}/>           
+      <MainNavigator.Screen name="SignUp" component={SignUp}/>
+      <MainNavigator.Screen name="ProductDetails" component={ProductDetails}/>
+      <MainNavigator.Screen name="Cart" component={Cart}/>
+      <MainNavigator.Screen name="CartCount" component={CartCount}/> 
+      <MainNavigator.Screen name="CartContext" component={CartContext}/>    
+      </MainNavigator.Navigator>           
+      </NavigationContainer>
+      </View>
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
